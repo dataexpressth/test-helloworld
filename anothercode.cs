adc280cdafce2ad6c6974c0f4c9787d2091746bd
pawarat-dx <96221013+pawarat-dx@users.noreply.github.com>
2024-02-16 09:50:31 +0700

    SSP-9 Update anothercode.cs
    #comment something

--- anothercode.cs
+++ anothercode.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-1234
+
     
 namespace CSharpTutorials
 {
@@ -15,6 +15,7 @@ namespace CSharpTutorials
             string message = "Hello World!!";
 
             Console.WriteLine(message);
+            //comment here 1234
         }
         
         
