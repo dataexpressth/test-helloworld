33e897540ed9c2760c13e02a30473afebbee7a1f
pawarat-dx <96221013+pawarat-dx@users.noreply.github.com>
2025-01-23 12:26:53 +0700

    SSP-17 Update helloworld.cs #comment text

--- helloworld.cs
+++ helloworld.cs
@@ -7,17 +7,5 @@ namespace HelloWorld
             //add comment
             System.Console.WriteLine("Hello World!");
         }
-
-        //
-        void AnotherFunction()
-        {
-            return;
-        }
-        //add comment for test
-        //
-        void AnotherFunction()
-        {
-            return;
-        }
     }
 }
